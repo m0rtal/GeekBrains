973470dddea5cbf9b69b17ba96baf106a01e95a0
m0rtal <alexander@nagorny.pro>
2020-11-23 22:19:47 +0300

    Урок 5 задания 1-5

--- lesson05.py
+++ lesson05.py
@@ -13,17 +13,21 @@
 # print(f"В считанном файле {filename} было строк: {len(text)}, слов: {sum([len(line.split()) for line in text])}")
 #
 # # Задание 03
-# import string
-# from random import choice, randint, random
+## import string
+from random import choice, randint, random
+
+
 # from math import fsum
 #
 #
-# def randomletters():
-#     """Генерирует случайный набор символов случайной длины"""
-#     # letters = string.ascii_lowercase  # хотел латинскими, но это скучно
-#     letters = "".join([chr(i) for i in range(ord('а'), ord('а') + 32)])
-#     length = randint(5, 15)
-#     return "".join(choice(letters) for _ in range(length)).capitalize()
+def randomletters():
+    """Генерирует случайный набор символов случайной длины"""
+    # letters = string.ascii_lowercase  # хотел латинскими, но это скучно
+    letters = "".join([chr(i) for i in range(ord('а'), ord('а') + 32)])
+    length = randint(5, 15)
+    return "".join(choice(letters) for _ in range(length)).capitalize()
+
+
 #
 #
 # def randomsalary():
@@ -35,10 +39,10 @@
 # # Сгенерируем файл с зарплатой
 # filename = "salary.txt"
 # for _ in range(100):
-#     with open(filename, "a") as fha:
+#     with open(filename, "a", encoding="utf-8") as fha:
 #         fha.write(f"{randomletters()} {randomsalary()}\n")
 #
-# with open(filename, "r") as fhr:
+# with open(filename, "r", encoding="utf-8") as fhr:
 #     salary = fhr.readlines()
 #
 # poverty = [line.split()[0] for line in salary if float(line.split()[1]) < 20000]
@@ -56,26 +60,51 @@
 #
 # translator = Translator(from_lang="English", to_lang="Russian")
 #
-# with open("four.txt", "r") as fhr:
+# with open("four.txt", "r", encoding="utf-8") as fhr:
 #     text_list = fhr.readlines()
 #
 # for line in text_list:
 #     words = line.split()
 #     words[0] = translator.translate(words[0])
 #     words = " ".join(words) + "\n"
-#     with open("четыре.txt", "a") as fha:
+#     with open("четыре.txt", "a", encoding="utf-8") as fha:
 #         fha.write(words)
 #
 # # Задание 05
 # filename = "numbers.txt"
 # numbers = " ".join([str(randint(0, 9)) for _ in range(1000)])
-# with open(filename, "a") as fha:
+# with open(filename, "a", encoding="utf-8") as fha:
 #     fha.write(numbers)
 #
-# with open(filename, "r") as fhr:
+# with open(filename, "r", encoding="utf-8") as fhr:
 #     text = fhr.read()
 #
 # print(f"Сумма чисел в файле {filename}: {fsum([float(number) for number in text.split()]):.0f}")
 #
 # Задание 06
+import re
 
+with open("schedule.txt", "r", encoding="utf-8") as fhr:
+    raw_text = fhr.readlines()
+
+schedule = {}
+for line in raw_text:
+    words = line.split()
+    course = words[0][:-1]
+    nums = map(int, [re.sub(r"\((.*?)\)", "", num).replace("-", "") for num in words[1:]])
+    print(nums)
+
+# не пригодилось, но жалко выбрасывать
+# records = set(v for key, value in loaded_json.items() for v in value)
+
+# summary = {key: sum(loaded_json[key].values()) for key, value in loaded_json.items()}
+# for key, value in summary.items():
+#     print(f"Общее количество занятий по предмету '{key}': {value}")
+
+# # Задание 07
+# ownership = ("ООО", "ИП", "КФХ", "АО", "ЗАО", "ОАО", "ОДО")
+#
+#
+#
+# firms = [[randomletters(), choice(ownership), randint(5000, 20000), randint(5000, 1000)] for _ in range(100)]
+# print(firms)
