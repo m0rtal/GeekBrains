5a85a19328f03f73ca8ad625e43919325a42196a
m0rtal <alexander@nagorny.pro>
2020-11-26 18:47:58 +0300

    Урок 5 все задания

--- lesson05.py
+++ lesson05.py
@@ -1,19 +1,19 @@
-# Задание 01
-filename = "text.txt"
-text = input("Введите текст для сохранения: ")
-while text != "":
-    with open(filename, "a", encoding="utf-8") as fha:
-        fha.write(text + "\n")
-    text = input("Введите текст для сохранения: ")
-
-# задание 02
-with open(filename, "r", encoding="utf-8") as fhr:
-    text = fhr.readlines()
-
-print(f"В считанном файле {filename} было строк: {len(text)}, слов: {sum([len(line.split()) for line in text])}")
-
-# Задание 03
-
+# # Задание 01
+# filename = "text.txt"
+# text = input("Введите текст для сохранения: ")
+# while text != "":
+#     with open(filename, "a", encoding="utf-8") as fha:
+#         fha.write(text + "\n")
+#     text = input("Введите текст для сохранения: ")
+#
+# # задание 02
+# with open(filename, "r", encoding="utf-8") as fhr:
+#     text = fhr.readlines()
+#
+# print(f"В считанном файле {filename} было строк: {len(text)}, слов: {sum([len(line.split()) for line in text])}")
+#
+# # Задание 03
+#
 from random import choice, randint, random
 
 from math import fsum
@@ -27,78 +27,80 @@ def randomletters():
     return "".join(choice(letters) for _ in range(length)).capitalize()
 
 
-def randomsalary():
-    """Генерирует случайное float число в диапазоне от ~1.000 до ~200.000"""
-    salary = random() * randint(100000, 200000)
-    return round(salary, 2)
-
-
-filename = "salary.txt"
-for _ in range(100):
-    with open(filename, "a", encoding="utf-8") as fha:
-        fha.write(f"{randomletters()} {randomsalary()}\n")
-
-with open(filename, "r", encoding="utf-8") as fhr:
-    salary = fhr.readlines()
-
-poverty = [line.split()[0] for line in salary if float(line.split()[1]) < 20000]
-print("Сотрудники, которым срочно нужно поднять зарплату:\n", "\n".join(poverty), sep="")
-
-total_salary = fsum([float(line.split()[1]) for line in salary])
-poverty_salaries_list = [float(line.split()[1]) for line in salary if float(line.split()[1]) < 20000]
-poverty_salary = fsum(poverty_salaries_list)
-print(f"Средняя зарплата персонала: {(total_salary / len(salary)):.2f}")
-print(f"Средняя зарплата нуждающегося персонала: {(poverty_salary / len(poverty_salaries_list)):.2f}")
-
-# Задание 04
-from translate import Translator
-
-translator = Translator(from_lang="English", to_lang="Russian")
-
-with open("four.txt", "r", encoding="utf-8") as fhr:
-    text_list = fhr.readlines()
-
-for line in text_list:
-    words = line.split()
-    words[0] = translator.translate(words[0])
-    words = " ".join(words) + "\n"
-    with open("четыре.txt", "a", encoding="utf-8") as fha:
-        fha.write(words)
-
-# Задание 05
-filename = "numbers.txt"
-numbers = " ".join([str(randint(0, 9)) for _ in range(1000)])
-with open(filename, "a", encoding="utf-8") as fha:
-    fha.write(numbers)
-
-with open(filename, "r", encoding="utf-8") as fhr:
-    text = fhr.read()
-
-print(f"Сумма чисел в файле {filename}: {fsum([float(number) for number in text.split()]):.0f}")
-
-# Задание 06
-from string import punctuation
-import re
-
-with open("schedule.txt", "r", encoding="utf-8") as fhr:
-    raw_text = fhr.readlines()
-
-schedule = {}
-for line in raw_text:
-    line = line.translate(str.maketrans("", "", punctuation))
-    words = line.split()
-    course = words[0]
-    # nums = [''.join(filter(str.isdigit, word)) for word in words[1:]]
-    nums = re.findall(r'\d+', line)
-    summ = sum(int(num) if num else 0 for num in nums)
-    schedule.update({course: summ})
-
-# не пригодилось, но жалко выбрасывать
-# records = set(v for key, value in loaded_json.items() for v in value)
-# summary = {key: sum(loaded_json[key].values()) for key, value in loaded_json.items()}
-
-for key, value in schedule.items():
-    print(f"Общее количество часов по предмету '{key}': {value}")
+#
+#
+# def randomsalary():
+#     """Генерирует случайное float число в диапазоне от ~1.000 до ~200.000"""
+#     salary = random() * randint(100000, 200000)
+#     return round(salary, 2)
+#
+#
+# filename = "salary.txt"
+# for _ in range(100):
+#     with open(filename, "a", encoding="utf-8") as fha:
+#         fha.write(f"{randomletters()} {randomsalary()}\n")
+#
+# with open(filename, "r", encoding="utf-8") as fhr:
+#     salary = fhr.readlines()
+#
+# poverty = [line.split()[0] for line in salary if float(line.split()[1]) < 20000]
+# print("Сотрудники, которым срочно нужно поднять зарплату:\n", "\n".join(poverty), sep="")
+#
+# total_salary = fsum([float(line.split()[1]) for line in salary])
+# poverty_salaries_list = [float(line.split()[1]) for line in salary if float(line.split()[1]) < 20000]
+# poverty_salary = fsum(poverty_salaries_list)
+# print(f"Средняя зарплата персонала: {(total_salary / len(salary)):.2f}")
+# print(f"Средняя зарплата нуждающегося персонала: {(poverty_salary / len(poverty_salaries_list)):.2f}")
+#
+# # Задание 04
+# from translate import Translator
+#
+# translator = Translator(from_lang="English", to_lang="Russian")
+#
+# with open("four.txt", "r", encoding="utf-8") as fhr:
+#     text_list = fhr.readlines()
+#
+# for line in text_list:
+#     words = line.split()
+#     words[0] = translator.translate(words[0])
+#     words = " ".join(words) + "\n"
+#     with open("четыре.txt", "a", encoding="utf-8") as fha:
+#         fha.write(words)
+#
+# # Задание 05
+# filename = "numbers.txt"
+# numbers = " ".join([str(randint(0, 9)) for _ in range(1000)])
+# with open(filename, "a", encoding="utf-8") as fha:
+#     fha.write(numbers)
+#
+# with open(filename, "r", encoding="utf-8") as fhr:
+#     text = fhr.read()
+#
+# print(f"Сумма чисел в файле {filename}: {fsum([float(number) for number in text.split()]):.0f}")
+#
+# # Задание 06
+# from string import punctuation
+# import re
+#
+# with open("schedule.txt", "r", encoding="utf-8") as fhr:
+#     raw_text = fhr.readlines()
+#
+# schedule = {}
+# for line in raw_text:
+#     line = line.translate(str.maketrans("", "", punctuation))
+#     words = line.split()
+#     course = words[0]
+#     # nums = [''.join(filter(str.isdigit, word)) for word in words[1:]]
+#     nums = re.findall(r'\d+', line)
+#     summ = sum(int(num) if num else 0 for num in nums)
+#     schedule.update({course: summ})
+#
+# # не пригодилось, но жалко выбрасывать
+# # records = set(v for key, value in loaded_json.items() for v in value)
+# # summary = {key: sum(loaded_json[key].values()) for key, value in loaded_json.items()}
+#
+# for key, value in schedule.items():
+#     print(f"Общее количество часов по предмету '{key}': {value}")
 
 # Задание 07
 import json
@@ -113,22 +115,14 @@ with open(filename, "w", encoding="utf-8") as fhw:
     fhw.writelines(firms)
 
 with open(filename, "r", encoding="utf-8") as fhr:
-    data = fhr.readlines()
+    report = {row.split()[0]: (int(row.split()[2]) - int(row.split()[3])) for row in fhr.readlines()}
 
-data = [line.split() for line in data]
-for line in data:
-    line[2] = int(line[2])
-    line[3] = int(line[3])
-    line.append(line[2] - line[3])
-
-avg_profit = sum([line[4] for line in data if line[4] > 0]) / len(data)
+avg_profit = sum(report.values())/len(report.values())
 print(f"Средняя прибыль без учёта убыточных компаний: {avg_profit}")
 
-firms_dict = {line[0]: line[4] for line in data}
-firms_sorted = dict(sorted(firms_dict.items(), key=lambda item: item[1]))
-firms = [firms_sorted, {"average_profit": avg_profit}]
-print("Подробно о прибылях компаний, сортировка от меньшего к большему:")
-print(firms)
+new_report = [report, {"average_profit": avg_profit}]
 
-with open("firs.json", "w", encoding="utf-8") as fhw:
-    json.dump(firms, fhw)
+print("Подробно о прибылях компаний:", new_report, sep="\n")
+
+with open("firms.json", "w", encoding="utf-8") as fhw:
+    json.dump(new_report, fhw, ensure_ascii=False)
