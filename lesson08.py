2bff169ac480273f963cda9e8ad345b14bbb64c3
m0rtal <alexander@nagorny.pro>
2020-12-06 20:46:11 +0300

    Урок 08 задание 1-2

--- lesson08.py
+++ lesson08.py
@@ -1,36 +1,56 @@
-class Date:
-    def __init__(self, date_str):
-        """должна принимать дату в виде строки формата «день-месяц-год»"""
-        if isinstance(date_str, str):
-            self.date_str = date_str
-            self.retrieve_date()
-        else:
-            print("Принимает дату в виде строки формата «день-месяц-год»")
+# class Date:
+#     def __init__(self, date_str):
+#         """должна принимать дату в виде строки формата «день-месяц-год»"""
+#         if isinstance(date_str, str):
+#             self.date_str = date_str
+#             self.retrieve_date()
+#         else:
+#             print("Принимает дату в виде строки формата «день-месяц-год»")
+#
+#     def retrieve_date(self):
+#         self.day, self.month, self.year = map(int, self.date_str.split("-"))
+#         if self.validate(self.day, self.month, self.year):
+#             print("Date is valid")
+#         else:
+#             print("Date is not valid")
+#
+#     @staticmethod
+#     def validate(day, month, year):
+#         if day in range(1, 32) and month in range(1, 13) and year > 0:
+#             return True
+#         else:
+#             return False
+#
+#     def __str__(self):
+#         return " ".join(map(str, (self.day, self.month, self.year)))
+#
+#
+# date = Date("11-11-2020")
+# print(date)
+#
+# date = Date("11-13-2020")
+# print(date)
+#
+# date = Date("32-11-2020")
+# print(date)
+#
 
-    def retrieve_date(self):
-        self.day, self.month, self.year = map(int, self.date_str.split("-"))
-        if self.validate(self.day, self.month, self.year):
-            print("Date is valid")
-        else:
-            print("Date is not valid")
-
-    @staticmethod
-    def validate(day, month, year):
-        if day in range(1, 32) and month in range(1, 13) and year > 0:
-            return True
-        else:
-            return False
-
-    def __str__(self):
-        return " ".join(map(str, (self.day, self.month, self.year)))
+# Задание 02
+class DivisionByZeroError(Exception):
+    def __init__(self, txt):
+        self.txt = txt
 
 
-date = Date("11-11-2020")
-print(date)
+def division(divisible, divider):
+    """Делит два числа и корректно обрабатывает деление на 0"""
+    try:
+        if divider == 0:
+            raise DivisionByZeroError("Нельзя делить на ноль!")
+    except DivisionByZeroError as err:
+        print(err)
+    else:
+        print(divisible / divider)
 
-date = Date("11-13-2020")
-print(date)
 
-date = Date("32-11-2020")
-print(date)
-
+division(3, 0)
+division(10, 2)
